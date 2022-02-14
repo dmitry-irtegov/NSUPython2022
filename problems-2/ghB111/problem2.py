9b7f0e9bedc51675366f2b9e17c88e477fa400b6
Roman <b111forjob@gmail.com>
2022-02-14 09:46:10 +0700

    problems-2/problem2

---
+++ problems-2/ghB111/problem2.py
@@ -0,0 +1,33 @@
+import re
+
+def getReverseDictByFileName(filename):
+
+    orig_dict = {}
+    with  open(filename, 'r') as orig_dict_file:
+        for line in orig_dict_file:
+            words = re.split(', | - ', line[:-1]) # there is hope for the last line to end in \n as well
+            orig = words[0]
+            translations = words[1:]
+            orig_dict[orig] = translations
+
+    new_dict = {}
+
+    for word, translations in orig_dict.items():
+        for translation in translations:
+            if not translation in new_dict:
+                new_dict[translation] = []
+            new_dict[translation].append(word)
+
+    return new_dict
+
+
+
+
+if __name__ == '__main__':
+    new_dict = getReverseDictByFileName('dict.txt')
+    print("Result is: ")
+    for key in sorted(new_dict.keys()):
+        translations = new_dict[key]
+        print(f"{key} - {', '.join(translations)}")
+    
+
