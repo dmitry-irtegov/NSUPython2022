e1af2020aedf996f52005ad96740338370c510e7
Titrom25 <titrom25@mail.ru>
2022-02-19 13:48:02 +0700

    Update problems-1/problem1

--- problems-1/r-titkov/problem1.py
+++ problems-1/r-titkov/problem1.py
@@ -1,10 +1,12 @@
-def cumSum(input):
+def cumSum(array):
 	sum_list = [0]
-	for num in input:
+	for num in array:
 		new_sum = sum_list[-1] + num
 		sum_list.append(new_sum)
 
 	return sum_list
 
 if __name__ == "__main__":
-	print(f'cumSum([1,2,3]) == [0, 1, 3, 6] is {cumSum([1,2,3]) == [0, 1, 3, 6]}')
+	print('Enter integers:', end=' ')
+	array = list(map(int, input().split()))
+	print(f'Result: {cumSum(array)}')
