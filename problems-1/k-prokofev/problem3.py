d65445d5e7f9c56718dc7b1005542b4272ff1352
Кирилл Прокофьев <kprokofev@65apps.com>
2022-02-21 10:10:37 +0700

    (Problems-1) task 3

---
+++ problems-1/k-prokofev/problem3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def collatz(number: int):
+    result = str(number)
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            result += ' --> ' + str(number)
+        else:
+            number = 3 * number + 1
+            result += ' --> ' + str(number)
+    return result
+
+
+if __name__ == '__main__':
+    a = int(input('Enter number: '))
+    result = collatz(a)
+    print(result)
