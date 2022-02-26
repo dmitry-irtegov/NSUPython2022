52ebfa85e4f6e24cf5ffd4de761ba0fa39af8b70
p.vasilev <p.vasilev@g.nsu.ru>
2022-02-27 03:38:20 +0700

    Add exception handling

--- problems-2/p-vasilev/problem3/problem3.py
+++ problems-2/p-vasilev/problem3/problem3.py
@@ -3,7 +3,8 @@
 import os.path
 import sys
 
-if __name__ == "__main__":
+
+def main():
     d: str
     # set current working directory as the directory if no parameters were passed
     if len(sys.argv) == 1:
@@ -11,11 +12,32 @@ if __name__ == "__main__":
     else:
         d = sys.argv[1]
 
-    files = [f for f in os.listdir(d)
-             if os.path.isfile(os.path.join(d, f))]
+    files = []
+    try:
+        files = [f for f in os.listdir(d)
+                 if os.path.isfile(os.path.join(d, f))]
+    except FileNotFoundError:
+        print(f'Directory "{d}" does not exist.', file=sys.stderr)
+        exit(1)
+    except NotADirectoryError:
+        print(f'"{d}" is a file, not a directory.', file=sys.stderr)
+        exit(1)
+    except PermissionError:
+        print(f'You don\'t have permission to access "{d}".')
+        exit(1)
+
     filesize_dict = {}
-    max_name_len = max([len(f) for f in files])
+    if len(files) == 0:
+        max_name_len = 0
+    else:
+        max_name_len = max([len(f) for f in files])
     for f in files:
         filesize_dict[f] = os.stat(os.path.join(d, f)).st_size
-    for i in sorted(sorted(filesize_dict.items(), key=lambda x: x[0]), key=lambda x: x[1], reverse=True):
+
+    filesize_dict = sorted(filesize_dict.items(), key=lambda x: x[0])
+    for i in sorted(filesize_dict, key=lambda x: x[1], reverse=True):
         print(i[0] + '-' * (max_name_len - len(i[0])), i[1], 'B')
+
+
+if __name__ == "__main__":
+    main()
