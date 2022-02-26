cc318052e35ea26d2ae80cca1212076f26dad212
p.vasilev <p.vasilev@g.nsu.ru>
2022-02-27 03:06:45 +0700

    Add exception handling

--- problems-1/p-vasilev/problem2/problem2.py
+++ problems-1/p-vasilev/problem2/problem2.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+import sys
+
 
 def problem2(num_list: [int], lower_bound: int = None, higher_bound: int = None) -> [int]:
     """Returns the input list that is limited by the given bounds"""
 
-    if lower_bound and higher_bound and lower_bound > higher_bound:
-        raise ValueError("Lower bound has to be lower than the higher bound")
+    try:
+        if lower_bound > higher_bound:
+            raise ValueError("Lower bound has to be lower than the higher bound")
+    except TypeError:
+        pass
 
     # check that lower_bound/higher_bound is set
     # then compare and set the element accordingly
@@ -13,11 +18,42 @@ def problem2(num_list: [int], lower_bound: int = None, higher_bound: int = None)
             i for i in num_list]
 
 
-if __name__ == '__main__':
+def main():
     print('Enter the input list (space-separated):')
-    input_list = [int(i) for i in input().split()]
+
+    while True:
+        try:
+            input_list = [int(i) for i in input().split()]
+            break
+        except ValueError:
+            print('Input should only contain integers. Try again.', file=sys.stderr)
+        except KeyboardInterrupt:
+            print()
+            exit(0)
+
     print('Enter the two bounds (space-separated, enter "-" to skip a bound):')
-    a = input().split()
-    b = int(a[1]) if a[1] != '-' else None
-    a = int(a[0]) if a[0] != '-' else None
-    print(*problem2(input_list, a, b))
+    while True:
+        try:
+            a = input().split()
+            b = int(a[1]) if a[1] != '-' else None
+            a = int(a[0]) if a[0] != '-' else None
+
+            if a and b and a > b:
+                print('The lower bound should go first, higher should go second. Try again.')
+                continue
+            
+            break
+        except ValueError:
+            print('The bounds should be integers or "-". Try again.', file=sys.stderr)
+        except IndexError:
+            print('Two bounds should be entered. Try again.', file=sys.stderr)
+        except KeyboardInterrupt:
+            print()
+            exit(0)
+
+    print('Answer: ')
+    print(problem2(input_list, a, b))
+
+
+if __name__ == '__main__':
+    main()
