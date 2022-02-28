f29571a0bc30009340c77788575c845c0b44c2d6
Кирилл Прокофьев <kprokofev@65apps.com>
2022-02-28 09:39:20 +0700

    Добавил тесты

--- problems-1/k-prokofev/problem1.py
+++ problems-1/k-prokofev/problem1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import pytest
+
 
 def cumulativeSums(list):
     result = [0]
@@ -17,3 +19,17 @@ if __name__ == '__main__':
         print('Result is:', result)
     except ValueError:
         print('Please enter integers. Try again.')
+
+
+class TestSums():
+    def test_1(self):
+        assert cumulativeSums([1, 3, 5, 7]) == [0, 1, 4, 9, 16]
+
+    def test_2(self):
+        assert cumulativeSums([0]) == [0, 0]
+
+    def test_3(self):
+        assert cumulativeSums([]) == [0]
+
+    def test_4(self):
+        assert cumulativeSums(range(1, 18)) == [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153]
