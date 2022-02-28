a5b27077cfa2564ce7809fea718d210144139e90
sescer <b.patrushev@g.nsu.ru>
2022-02-28 09:12:26 +0700

    Add exception

--- problems-1/b-patrushev/problem5.py
+++ problems-1/b-patrushev/problem5.py
@@ -3,6 +3,7 @@
 This module implements solution for problem 5 in Problems-1 [1]
 [1]: http://parallels.nsu.ru/~fat/Python/problems-1.pdf
 """
+from math import sqrt
 
 
 def primes(num):
@@ -12,7 +13,7 @@ def primes(num):
     :return: a list in which each prime factor p and its degree k corresponds to the pair (p, k)
     """
     res = []
-    for p in range(2, int(num ** (1/2) + 1)):
+    for p in range(2, int(sqrt(num) + 1)):
         k = 0
         while num % p == 0:
             num //= p
@@ -23,6 +24,15 @@ def primes(num):
         res.append([num, 1])
     return res if len(res) > 0 else [[num, 1]]
 
+
 if __name__ == '__main__':
-    print("Enter number to factorize into prime numbers")
-    print(primes(int(input())))
+    while True:
+        try:
+            num = int(input("Enter number to factorize into prime numbers: "))
+            if num < 1:
+                print("Sorry, input must be a positive integer. Try again")
+                continue
+            print(primes(num))
+            break
+        except ValueError as e:
+            print(f"Invalid input: \n{e} \nTry again")
