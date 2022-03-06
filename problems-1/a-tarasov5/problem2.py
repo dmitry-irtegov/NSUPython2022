f014d9b59061cba679874c71845577b3536a5452
Artem <artemtarasov678@gmail.com>
2022-03-07 06:01:29 +0700

    add error handle

--- problems-1/a-tarasov5/problem2.py
+++ problems-1/a-tarasov5/problem2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import sys
+
 
 def trim_nums(l, l_bound, up_bound):
     """
@@ -16,7 +18,18 @@ def trim_nums(l, l_bound, up_bound):
 
 if __name__ == "__main__":
     print("Enter numbers to trim (divided by whitespace):")
-    l = [int(elem) for elem in input().split()]
-    print("Enter lower bound and upper bound divided by space:")
-    lower_bound, upper_bound = [int(elem) for elem in input().split()]
-    print(trim_nums(l, lower_bound, upper_bound))
+    try:
+        l = [int(elem) for elem in input().split()]
+        print("Enter lower bound and upper bound")
+
+        lower_bound, upper_bound = [int(elem) for elem in input().split()]
+
+        if lower_bound > upper_bound:
+            print("lower bound must be less or equal then upper")
+            exit(1)
+
+        print(trim_nums(l, lower_bound, upper_bound))
+
+    except ValueError:
+        print("You must provide only numbers divided by space", file=sys.stderr)
+        exit(1)
