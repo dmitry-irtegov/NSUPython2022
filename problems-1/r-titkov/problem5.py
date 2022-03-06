7d6e2f451d0f83213fd9819a3a553ffa8761544b
Titrom25 <titrom25@mail.ru>
2022-03-06 13:07:01 +0300

    Add problems-1/problem5

---
+++ problems-1/r-titkov/problem5.py
@@ -0,0 +1,45 @@
+import sys
+
+def factorize(number):
+	"""
+	The function takes a number to factorize and
+	returns the factorization for the given number
+
+	:param number: The number to factorize
+	:return: The factorization for the given number
+	"""
+
+	if number == 1:
+		return [[1, 1]]
+
+	factorization = []
+	for divider in range(2, number + 1):
+		degree = 0
+
+		while (number % divider) == 0:
+			number //= divider
+			degree += 1
+
+		if degree > 0:
+			factorization.append([divider, degree])
+		
+		if number == 1:
+			break
+
+	return factorization
+
+
+if __name__ == '__main__':
+	try:
+		print("Enter a number to factorize: ", end='')
+		number = int(input())
+		if number < 1:
+			sys.exit("Number must be greater than zero")
+
+		print(factorize(number))
+	except KeyboardInterrupt:
+		sys.exit("\nKeyboardInterrupt occured")
+	except EOFError:
+		sys.exit("\nEOFError occured")
+	except OSError as e:
+		sys.exit(f"\nEOFError occured: {e.strerror}")
