f7d4f0eafdc04b80b082d672ed82ecc2eedaead8
kymaka <seafth345@gmail.com>
2022-03-20 19:50:54 +0700

    Updated error handling and added unittests

--- problems-1/i-shatalov/problem1.py
+++ problems-1/i-shatalov/problem1.py
@@ -1,3 +1,7 @@
+import sys
+import unittest
+
+
 def cumulative(array):
     res = 0
     tmpArr = [0]
@@ -7,11 +11,36 @@ def cumulative(array):
     return tmpArr
 
 
+class TestCumulative(unittest.TestCase):
+
+    def test_three(self):
+        self.assertEqual(cumulative([1, 2, 3]), [0, 1, 3, 6])
+
+    def test_four(self):
+        self.assertEqual(cumulative([1, 2, 3, 4]), [0, 1, 3, 6, 10])
+
+    def test_ten(self):
+        self.assertEqual(cumulative([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55])
+
+
 if __name__ == "__main__":
-    try:
-        input_array = [int(value) for value in input("Please enter array of numbers: ").split()]
-        print(cumulative(input_array))
-    except ValueError as e:
-        print("Error: ", e)
-    except BaseException:
-        print("Error! Please enter array of integer numbers separated with space.")
+    if "--unittest" in sys.argv:
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestCumulative)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+        exit()
+
+    while True:
+        try:
+            input_array = [int(value) for value in input("Please enter array of numbers: ").split()]
+            print(cumulative(input_array))
+            exit()
+        except ValueError as e:
+            print("Error: ", e)
+        except KeyboardInterrupt:
+            print("Keyboard interrupt")
+            exit()
+        except IOError:
+            print("Some Error occurred. Please enter valid integer number.")
+        except Exception:
+            print("Unexpected error occurred.")
+            exit()
