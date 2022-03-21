665f333e0a7707c7f1ebbbe1724985c1d238857a
Кирилл Прокофьев <kprokofev@65apps.com>
2022-03-21 10:25:13 +0700

    Дополнил обработку исключение + добавил BadCases в тестах

--- problems-1/k-prokofev/problem5.py
+++ problems-1/k-prokofev/problem5.py
@@ -7,7 +7,13 @@ def factor(number):
     result = []
     multiplier = 2
     if not isinstance(number, int):
-        return []
+        raise ValueError("Number must be an Int.")
+
+    if number < 1:
+        raise ValueError("Number must be > zero.")
+
+    if number == 1:
+        return [[1, 1]]
 
     while multiplier * multiplier <= number:
         pow = 0
@@ -30,23 +36,35 @@ if __name__ == '__main__':
             result = factor(a)
             print(result)
             break
-        except ValueError as e:
-            print('Please enter an integer. ', sys.stderr)
-        print('Tests:', unittest.main())
+        except ValueError:
+            print("\nValue error received")
+            sys.exit()
+        except KeyboardInterrupt:
+            print("\nKeyboardInterrupt received")
+            sys.exit()
+        except EOFError:
+            print("\nEOFError received")
+            sys.exit()
+        except Exception as e:
+            sys.exit(f"Error: {e}")
+
+    print('Tests:', unittest.main())
 
 
 class TestFactor(unittest.TestCase):
     def test_0(self):
-        self.assertEqual(factor(0), [])
+        self.assertRaises(ValueError, factor, 0)
 
     def test_1(self):
         self.assertEqual(factor(1), [[1, 1]])
 
     def test_2(self):
-        self.assertEqual(factor('someText'), [])
-
-    def test_3(self):
         self.assertEqual(factor(1000000000000), [[2, 12], [5, 12], [1, 1]])
 
+    def test_3(self):
+        self.assertRaises(ValueError, factor, -1000000000000)
+
     def test_4(self):
-        self.assertEqual(factor(-100), [])
+        self.assertRaises(ValueError, factor, 'someText')
+
+
