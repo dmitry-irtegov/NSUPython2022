f8ab6248172d32cff0e2b7b0d118c70a5cd82ded
Roman <b111forjob@gmail.com>
2022-04-18 10:04:44 +0700

    Add sys.exit on unknown exception

--- problems-4/r-brek/problem-2/tr/__main__.py
+++ problems-4/r-brek/problem-2/tr/__main__.py
@@ -125,6 +125,7 @@ def main():
     except Exception as e:
         print(
             f"Error: unexpected error during execution: {e}", file=sys.stderr)
+        sys.exit(3)
 
 
 if __name__ == "__main__":
