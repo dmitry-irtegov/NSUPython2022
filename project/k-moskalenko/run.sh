2c49f396d9da8f588a63224f88337d2ac93b2232
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2022-05-22 23:49:53 +0700

    Update the run script

--- project/k-moskalenko/run.sh
+++ project/k-moskalenko/run.sh
@@ -1,3 +1,7 @@
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade Pillow
+
 mkdir build
 cd build || exit
 
@@ -5,4 +9,4 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --clean-first
 
 cd ../src || exit
-python -m python
+python3 -m python
