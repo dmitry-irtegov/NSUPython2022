749b9d8e24632c92897e209e705856528d28e73f
sescer <b.patrushev@g.nsu.ru>
2022-06-06 10:03:28 +0700

    Fix setup_env.sh

--- project/b-patrushev/setup_env.sh
+++ project/b-patrushev/setup_env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  apt update
-  apt install -y python3-pip python3-venv python3-tk
+  sudo apt update
+  sudo apt install -y python3-pip python3-venv python3-tk
 fi
 
 python3 -m venv .venv
